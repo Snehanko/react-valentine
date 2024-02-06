5a90333b0c89c09dbf8d45cf8919ddbe302d5b89
Darx <snehankobose31@gmail.com>
2024-02-06 23:41:41 +0530

    Addedbase code for the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,54 @@
 import { useState } from 'react'
 import './App.css'
 
+const phrases = [
+  "No.",
+  "Are You sure",
+  "Really sure?",
+  "Pookie please",
+  "Don't do this to me",
+  "I'm gonan cry....",
+  "You're breaking my heart heart ;(",
+];
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [noCount, setNoCount] = useState(0)
+  const [yesPressed, setYesPressed] = useState(false)
+  const yesButtonSize = noCount * 20 + 16;
 
   return (
-    <>
-      <h1 className='text-3xl font-bold'>React App Valentines day</h1>
-    </>
+    <div className='valentine'>
+      {
+        yesPressed ? (<>
+          <img
+            alt='bear-kissing'
+            src="https://tenor.com/view/bear-kiss-bear-kisses-kiss-kisses-love-gif-22536058.gif"
+          />
+          <div className='text'>Yayyy!!!</div>
+        </>)
+          :
+          (<>
+            <img
+              alt='sorry-feeling-bear'
+              src='https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif'
+            />
+
+            <div>Will you be my valentine?</div>
+            <div>
+              <button
+                className='yesButton'
+                style={{ fontSize: yesButtonSize }}
+                onClick={() => setYesPressed(true)}>
+                Yes
+              </button>
+              <button onClick={handleClick}
+                className='noButton'>
+                No
+              </button>
+            </div>
+          </>)
+      }
+    </div>
   )
 }
 
